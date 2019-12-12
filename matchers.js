42e91b39fa12027cc8a4d5046201943ece21ec13
Jiabin Geng <gengjiabin@gmail.com>
2019-12-12 15:12:48 -0700

    Update matchers.js

--- matchers.js
+++ matchers.js
@@ -2,7 +2,7 @@ function multiply_matcher(data, key, valuesArray) {
     if(data["factor1"] && data["factor2"] && data["factor1"] * data["factor2"] > 100){
         return true;
     }
-    return fasle;
+    return false;
 }
 
 EXTENDED_MATCHER_MAP.set('multiply_matcher', multiply_matcher);

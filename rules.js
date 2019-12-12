3dc00ce819e1649bcf64d41a77a47f2de7cf9e00
Jiabin Geng <gengjiabin@gmail.com>
2019-12-12 10:32:50 -0700

    Create rules.js

---
+++ rules.js
@@ -0,0 +1,61 @@
+rules = {
+    "version": 1,
+    "rules": [
+        {
+            "condition": {
+                "type": "group",
+                "definition": {
+                    "logic": "and",
+                    "conditions": [
+                        {
+                            "type": "group",
+                            "definition": {
+                                "logic": "or",
+                                "conditions": [
+                                    {
+                                        "type": "group",
+                                        "definition": {
+                                            "logic": "and",
+                                            "conditions": [
+                                                {
+                                                    "type": "matcher",
+                                                    "definition": {
+                                                        "key": "~type",
+                                                        "matcher": "eq",
+                                                        "values": [
+                                                            "com.adobe.eventType.generic.track"
+                                                        ]
+                                                    }
+                                                },
+                                                {
+                                                    "type": "matcher",
+                                                    "definition": {
+                                                        "key": "~source",
+                                                        "matcher": "eq",
+                                                        "values": [
+                                                            "com.adobe.eventSource.requestContent"
+                                                        ]
+                                                    }
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            },
+            "consequences": [
+                {
+                    "id": "RC84399d1d37764f61a16ed4c2c66fb3a7",
+                    "type": "pb",
+                    "detail": {
+                        "timeout": 0,
+                        "templateurl": "https://www.adobe.com"
+                    }
+                }
+            ]
+        }
+    ]
+}

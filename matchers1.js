43a876ec7ddba9bc9f192b0d826d84f5114dcab4
Jiabin Geng <gengjiabin@gmail.com>
2019-12-13 09:44:42 -0700

    Create matchers1.js

---
+++ matchers1.js
@@ -0,0 +1,9 @@
+function multiply_matcher(data, key, valuesArray) {
+    if(data["data"] && data["data"]["contextdata"] && data["data"]["contextdata"]["factor1"] && data["data"]["contextdata"]["factor2"]
+       && data["data"]["contextdata"]["factor1"] * data["data"]["contextdata"]["factor2"] > 100){
+        return true;
+    }
+    return false;
+}
+
+EXTENDED_MATCHER_MAP.set('multiply_matcher', multiply_matcher);
